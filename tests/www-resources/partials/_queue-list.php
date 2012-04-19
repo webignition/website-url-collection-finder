b1a780e607d56af358ab81884a9efb18d579a069
Jon Cram <jon@webignition.net>
2012-04-19 20:07:25 +0100

    Updated test interface, put new and processed queues side by side

--- tests/www-resources/partials/_queue-list.php
+++ tests/www-resources/partials/_queue-list.php
@@ -6,6 +6,7 @@ echo '<h2>Queue contents:</h2>';
 
 
 $newQueueContents = $controller->queue('new')->contents();
+echo '<div class="queue queue-new">';
 echo '<h3>New (<span id="new-queue-count">'.count($newQueueContents).'</span>)</h3>';
 if (count($newQueueContents)) {
     echo '<ul id="new-queue-list">';
@@ -18,9 +19,11 @@ if (count($newQueueContents)) {
 } else {
     echo '<p>Job finished - hit cancel above to start again</p>'; 
 }
+echo '</div>';
 
 
 $processedQueueContents = $controller->queue('processed')->contents();
+echo '<div class="queue queue-processed">';
 echo '<h3>Processed (<span id="processed-queue-count">'.count($processedQueueContents).'</span>)</h3>';
 echo '<ul id="processed-queue-list">';
 
@@ -29,3 +32,4 @@ foreach ($processedQueueContents as $queueItem) {
 }    
 
 echo '</ul>';
+echo '</div>';
